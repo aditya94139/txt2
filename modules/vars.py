ea03faa16115b7442a3cf252d397c9e5c01eb0f4
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-14 13:40:50 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,4 @@
 api_id = "29953693"
 api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
 bot_token = "6891781072:AAFXrq_ldpUFY9JPG2fgQioL_1TR_INxteg"
-auth_users = "841021123"
 auth_groups = "-1002046435003"
