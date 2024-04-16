9094dd3e29756d576ce7495f7e193e50537c0a52
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-17 01:58:16 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = "29953693"
 api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "6891781072:AAFXrq_ldpUFY9JPG2fgQioL_1TR_INxteg"
+bot_token = "7159297784:AAF6bFQAFljtJGHNFohKlr3lcK-CAjsmMUM"
 auth_groups = "-1002046435003"
