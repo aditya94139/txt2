2ec2ec19513eb3e7cf36a6de3c13a551b3f85f5b
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-20 08:25:33 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -26,13 +26,14 @@ bot = Client(
     "bot",
     api_id=api_id,
     api_hash=api_hash,
-    bot_token=bot_token)
+    bot_token=bot_token, 
+    sudo_users=ADMINS)
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
 async def account_login(bot: Client, m: Message):
     user = m.from_user.id if m.from_user is not None else None
-    if user is not None and user not in ADMINS :
+    if user is not None and user not in sudo_users:
         await m.reply("**BOT TERE LIYE NHI BANAYA, JA NIKAL KHUD BHI KUCHH KAR LE MEHNAT**", quote=True)
         return
     else:
