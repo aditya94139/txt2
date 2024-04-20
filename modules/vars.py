3adb869b8c32551b267f426d28c8a1aea49471f6
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-20 07:38:31 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,5 +3,8 @@ import os
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
-sudo_user = int(os.environ.get("SUDO_USERS"))
-log_channel = int(os.environ.get("GROUPS"))
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "841021123").split()):
+        ADMINS.append(int(x))
