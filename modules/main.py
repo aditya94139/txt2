4c7148c0774130001d3ab6103c48b2f74da9ecfe
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-20 07:42:06 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -9,7 +9,7 @@ import subprocess
 
 import core as helper
 from utils import progress_bar
-from vars import api_id, api_hash, bot_token, sudo_users, log_channel 
+from vars import *
 from aiohttp import ClientSession
 from pyromod import listen
 from subprocess import getstatusoutput
@@ -26,12 +26,10 @@ bot = Client(
     "bot",
     api_id=api_id,
     api_hash=api_hash,
-    bot_token=bot_token, 
-    sudo_users=sudo_users,
-    log_channel=log_channel)
+    bot_token=bot_token)
 
 
-@bot.on_message(filters.command(["start"]) & filters.user(log_channel))
+@bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("Hey Freind 😎 I Am A Bot For Download Links From Your **.TXT** File. \n\n **Bot Made By Surya Bishnoi 🚩** \n Send me /BISHNOI Command And Follow Steps..")
 
@@ -43,7 +41,7 @@ async def restart_handler(_, m):
 
 
 
-@bot.on_message(filters.command(["BISHNOI"]) & filters.user(log_channel))
+@bot.on_message(filters.command(["BISHNOI"]) & filters.user(ADMINS))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text('Send me **TXT File**⚡️')
     input: Message = await bot.listen(editable.chat.id)
