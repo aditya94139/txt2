ab1f8c0f565050951217a330976878249a088d43
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-20 08:14:19 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -31,7 +31,12 @@ bot = Client(
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text("Hey Freind 😎 I Am A Bot For Download Links From Your **.TXT** File. \n\n **Bot Made By Surya Bishnoi 🚩** \n Send me /BISHNOI Command And Follow Steps..")
+    user = m.from_user.id if m.from_user is not None else None
+    if user is not None and user not in (ADMINS) :
+        await m.reply("**BOT TERE LIYE NHI BANAYA, JA NIKAL KHUD BHI KUCHH KAR LE MEHNAT**", quote=True)
+        return
+    else:
+        editable = await m.reply_text("Hey Freind 😎 I Am A Bot For Download Links From Your **.TXT** File. \n\n **Bot Made By Surya Bishnoi 🚩** \n Send me /BISHNOI Command And Follow Steps..")
 
 
 @bot.on_message(filters.command("stop"))
