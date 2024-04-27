343a8844e585c47b07b1088969bb65b5ae9366c4
Sursa2929 <166400821+Sursa2929@users.noreply.github.com>
2024-04-27 07:19:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,11 +5,11 @@ api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 
 
-OWNER = int(os.environ.get("OWNER", 841021123))
+OWNER = int(os.environ.get("OWNER", '841021123,6775997216'))
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "841021123").split()):
+    for x in (os.environ.get("ADMINS", '841021123,6775997216').split(',')):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
