03e2ecadebbac107f07b6f950c033ef5402df2a0
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-05-13 09:18:16 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ import os
 
 api_id = int(os.environ.get("API_ID", '29251040'))
 api_hash = os.environ.get("API_HASH", 'a1de34e4ad21cc9fb34798473eaf7bab')
-bot_token = os.environ.get("BOT_TOKEN", '7049822434:AAGIoULkWi5FdZU9-Fw2PUfy6XLbI7FClfg')
+bot_token = os.environ.get("BOT_TOKEN", '7185221162:AAHS9nRL-YuGINtSGYO6y_mc0pvOBqwcfdM')
 
 
 OWNER = int(os.environ.get("OWNER", '1280494242'))
